aff70041eb369a6fc9932c7499f55cdf6afd6854
David Henrique <dh210605@gmail.com>
2024-05-16 19:07:16 -0300

    teste

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.get('/', (req, res) => {
   res.send('Olá, mundo!');
+});
+
+app.get('/api/data', (req, res) => {
+  res.send('teste de back');
 });
 
 app.listen(port, () => {
